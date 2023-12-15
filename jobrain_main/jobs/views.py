aed7be20125cd63b3b0f50d16f4f162e5ad469aa
Hilal38 <hhilalkocak@gmail.com>
2023-12-15 21:48:26 +0300

    add highlight to searh words in app

--- jobrain_main/jobs/views.py
+++ jobrain_main/jobs/views.py
@@ -11,7 +11,25 @@ from django.db.models import Q
 # paganition system
 from django.core.paginator import Paginator, EmptyPage
 
+from django.utils.html import mark_safe
+from django.utils.html import escape
+import re
 
+def highlight_search_term(text, search_term):
+    # Öncelikle metin ve arama terimi HTML injection'a karşı escape edilir.
+    text = escape(text)
+    search_term = escape(search_term)
+    
+    # Arama terimini vurgulamak için HTML mark-up kullanılacak.
+    highlighted = re.sub(
+        f"({search_term})", 
+        r"<mark>\1</mark>", 
+        text, 
+        flags=re.IGNORECASE
+    )
+    
+    # Güvenli bir string olarak işaretleyip geri dönülür.
+    return mark_safe(highlighted)
 # Create your views here.
 def job_list(request):
     jobs = Job.objects.all().order_by('-date')
@@ -42,8 +60,15 @@ def job_list(request):
     except EmptyPage:
         jobs = paginator.page(paginator.num_pages)
 
+    highlighted_jobs = []
+    for job in jobs:
+        job.name = highlight_search_term(job.name, search_query)
+        job.description = highlight_search_term(job.description, search_query)
+        job.category.name = highlight_search_term(job.category.name, search_query)
+        highlighted_jobs.append(job)
+
     context = {
-        'jobs': jobs,
+        'jobs': highlighted_jobs,
         'categories': categories,
         'locations': locations,
         'jobtypes': jobtypes,
@@ -53,6 +78,7 @@ def job_list(request):
         'my_filter': my_filter,
         'search_query': search_query,
     }
+    
 
     return render(request, 'jobs.html', context)
 
